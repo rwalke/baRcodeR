d590503cf2b640e71a23990bfa63bfd3efc1804e
nine2k <13syeb@queensu.ca>
2017-04-27 14:34:39 -0400

    Update main.R

--- main.R
+++ main.R
@@ -8,13 +8,16 @@
 # Set the working directory (default will be the current directory)
 
 setwd("/Users/emilybao/Documents/courses/barcodeProject")
-
+##multi level label run:
 #source("LabelHierarchy.R") 
-cust <- toupper(readline("Do you want customize your own values for pdf layout? (y/n): "))
+
+##one level label run:
+#source("LabelMaker.R") 
+cust <- toupper(readline("Do you want to use the standard pdf layout? (y/n): "))
 if (cust == "Y" || cust == "y"){
-  source("customSizePDF.R")
-} else {
   source("createPDF.R")
+} else {
+  source("customSizePDF.R")
 }
 
 
