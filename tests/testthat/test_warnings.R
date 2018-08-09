3a49c6d9b0a015ec060b7b61c387d2b9cce8baff
yihanwu <yihan.wu@queensu.ca>
2018-08-09 13:22:45 -0400

    Added new testthat functions

--- tests/testthat/test_warnings.R
+++ tests/testthat/test_warnings.R
@@ -4,8 +4,12 @@ context("Input warnings")
 
 test_that("input errors are produced", {
   expect_error(label_maker(string="example", level="hi"), "level is not a string of numbers")
-  expect_error(label_maker(string="example", level=1:300, digits=2), "Max value of levels is greater than number of digits. Increase digits value")
+  expect_error(label_maker(string="example", level = c(1:5), digits="a"), "Digits is not a numerical value")
+  expect_warning(label_maker(string="example", level=1:300, digits=2), "Digits specified less than max number. Increasing number of digits.")
   expect_error(label_hier_maker(hierarchy = c("a",3,6)), "hierarchy is not in list format")
   expect_error(label_hier_maker(hierarchy = list(c("a",3,6), c("b",1))), "hierarchy entries are not of equal length. Each element should have a string, a beginning value and an end value.")
+  expect_error(label_hier_maker(hierarchy = list(c("a",3,6), c("b",1,"j"))), "Invalid ending number on level")
+  expect_error(label_hier_maker(hierarchy = list(c("a",3,6), c("b","j",3)), "Invalid starting number"))
+  expect_warning(label_hier_maker(hierarchy = list(c("a",3,6), c("b",100,103)), digits = 2), "Digits specified less than max level number")
   expect_error(create_PDF(), "Labels do not exist. Please pass in Labels")
 })
