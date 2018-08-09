39b08b10b8cbff2d75940e0ff8e3f0ed86e3709f
yihanwu <yihan.wu@queensu.ca>
2018-08-09 16:31:10 -0400

    spacing formatting and commenting

--- R/LabelHierarchy.R
+++ R/LabelHierarchy.R
@@ -17,7 +17,7 @@
 #'
 #' \dontrun{
 #' ## for interactive mode
-#' label_hier_maker(user=T)
+#' label_hier_maker(user = T)
 #' }
 #'
 #' ## how to make hierarchy list
@@ -25,41 +25,42 @@
 #' ## create vectors for each level in the order string_prefix, beginning_value,
 #' ## end_value and combine in list
 #'
-#' a<-c("a",3,6)
-#' b<-c("b",1,3)
-#' c<-list(a,b)
-#' Labels<-label_hier_maker(hierarchy=c)
+#' a <- c("a", 3, 6)
+#' b <- c("b", 1, 3)
+#' c <- list(a, b)
+#' Labels <- label_hier_maker(hierarchy = c)
 #' Labels
 #'
 #' ## add string at end of each label
-#' Labels<-label_hier_maker(hierarchy=c, end="end")
+#' Labels <- label_hier_maker(hierarchy = c, end = "end")
 #' Labels
 #'
 
 
-label_hier_maker <- function(user=F, hierarchy, end=NULL, digits=2){
-  if(user==T){
+label_hier_maker <- function(user = F, hierarchy, end = NULL, digits = 2){
+  # user interaction code
+  if(user == T){
     hlevels <- readline("What is the # of levels in hierarchy: ")
-    hlevels<-as.numeric(hlevels)
+    hlevels <- as.numeric(hlevels)
     # possible inputs
-    strEndCheck<-c("Y","y","N","n")
+    strEndCheck <- c("Y", "y", "N", "n")
     strEnd <- readline("String at end of label? (y/n) ")
     # check input
-    while((strEnd %in% strEndCheck )== FALSE){
-      print ("Invalid input, please only enter what is specified.")
+    while((strEnd %in% strEndCheck) == FALSE){
+      print("Invalid input, please only enter what is specified.")
       strEnd <- readline("String at end? (y/n) ")
     }
-    if (paste(strEnd) =="y"){
+    if (paste(strEnd) == "y"){
       end <- readline("Please enter ending string: ")
     } else {
       end <- ""
     }
     digits <- as.numeric(readline(paste0("Number of digits to print: ")))
-    while(is.na(as.numeric(digits))==T){
+    while(is.na(as.numeric(digits)) == T){
       print("Invalid input. Please only enter an integer.")
       digits <- as.numeric(readline(paste0("Number of digits to print: ")))
     }
-    hierarchy<-vector("list", hlevels)
+    hierarchy <- vector("list", hlevels)
     for(i in 1:hlevels){
       str <- readline(paste0("Please enter string for level ",i,": "))
       # startNum must be smaller than endNum
@@ -76,47 +77,52 @@ label_hier_maker <- function(user=F, hierarchy, end=NULL, digits=2){
       maxNum <- max(startNum,endNum)
       hierarchy[[i]]<-c(str, startNum, endNum)
     }
-  }
+  } # end user input
+  # hierarchy format check
   if (is.list(hierarchy) == F) stop("hierarchy is not in list format. See ?label_hier_maker")
   if (length(unique(sapply(hierarchy, length))) != 1) stop("hierarchy entries are not of equal length. Each element should have a string, a beginning value and an end value.")
+  # loop through hierarchy to generate text
   for(i in 1:length(hierarchy)){
-    str<-hierarchy[[i]][1]
-    startNum<-as.numeric(hierarchy[[i]][2])
-    endNum<-as.numeric(hierarchy[[i]][3])
+    str <- hierarchy[[i]][1]
+    startNum <- as.numeric(hierarchy[[i]][2])
+    endNum <- as.numeric(hierarchy[[i]][3])
     if (is.na(startNum) == T ){
       stop(paste0("Invalid starting number on level", i, ". Please doublecheck your input"))
     }
     if (is.na(endNum) == T ){
       stop(paste0("Invalid ending number on level", i, ". Please doublecheck your input"))
     }
-    maxNum <- max(startNum,endNum)
-    digitsMax <- max(digits,nchar(paste(maxNum)))
+    maxNum <- max(startNum, endNum)
+    digitsMax <- max(digits, nchar(paste(maxNum)))
     if (digitsMax > digits){
       warning("Digits specified less than max level number. Increasing number of digits for level")
-      digits<-digitsMax
+      digits <- digitsMax
     }
-    lvlRange <-c(startNum:endNum)
-    line<-paste0(str,"%0",digits,"d")
-    Labels<-sprintf(line,rep(lvlRange))
+    lvlRange <- c(startNum:endNum)
+    line <- paste0(str, "%0", digits, "d")
+    Labels <- sprintf(line, rep(lvlRange))
     if (i > 1) {
       ## replicate previous labels by number of elements in present level
-      temp_barcode<-rep(barcodes, each=length(startNum:endNum))
+      temp_barcode <- rep(barcodes, each = length(startNum:endNum))
       ## rep present level elements so the length matches that of temp_barcode
-      temp_label<-rep(Labels, length.out=length(temp_barcode))
+      temp_label <- rep(Labels, length.out = length(temp_barcode))
       ## paste everything together
-      Labels<-paste(temp_barcode, temp_label, sep="-")
+      Labels <- paste(temp_barcode, temp_label, sep = "-")
 
     }
     barcodes <- Labels
-  }
-  label_df<-cbind(barcodes, data.frame(t(sapply(strsplit(barcodes, "-"),c))))
-  df_names<-sapply(hierarchy, function(x) x[1])
+  } # end hierarchy making loop
+  # makes columns out of hierarchy levels
+  label_df <- cbind(barcodes, data.frame(t(sapply(strsplit(barcodes, "-"),c))))
+  df_names <- sapply(hierarchy, function(x) x[1])
+  # different ways to name the data frame columns depending on presence of level strings.
   if (any(nchar(df_names) == 0)){
     warning("Empty string in level. Using default column naming.")
-    names(label_df)<-c("label", paste0("level", 1:length(hierarchy)))
+    names(label_df) <- c("label", paste0("level", 1:length(hierarchy)))
   } else {
-    names(label_df)<-c("label", df_names)
+    names(label_df) <- c("label", df_names)
   }
+  # dont forget to add the string at the end
   label_df$label <- paste0(label_df$label, end)
   return(label_df)
 }
