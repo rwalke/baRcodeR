436e4cdc304f81de002b1bee53bf7b6cb119559a
yihanwu <yihan.wu@queensu.ca>
2019-10-17 20:10:04 -0400

    update shinytest to go with new shiny appearance

--- tests/testthat/test-label_generation.R
+++ tests/testthat/test-label_generation.R
@@ -8,6 +8,10 @@ test_that("labels are generated properly", {
     uniqID_maker(string="example", level=c(3,1)), 
     data.frame(label = c("example003", "example001"), ind_string = c("example", "example"), ind_number = c("003", "001")))
   expect_identical(
+    uniqID_maker(string = "example", level = 1:2, ending_string = "end"),
+    data.frame(label = c("example001-end", "example002-end"), ind_string = c("example", "example"), ind_number = c("001", "002"), end_string = c("end", "end"))
+  )
+  expect_identical(
     uniqID_maker(string="example", level=1:2, digits = 1), 
     data.frame(label = c("example1", "example2"), ind_string = c("example", "example"), ind_number = c("1", "2")))
   example_hier_df <- data.frame(label = c("a04-b01", "a04-b02", "a05-b01", "a05-b02"), a = c("a04", "a04", "a05", "a05"), b = c("b01", "b02", "b01", "b02"))
